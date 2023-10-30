13a8e2d65ed705f24b3ee2cd525b5eba01a1ca49
fabiancord <fabianarielcordobes@gmail.com>
2023-10-30 19:08:45 -0300

    actualizacion imagenes

--- client/src/Utils/revestimientosData.js
+++ client/src/Utils/revestimientosData.js
@@ -1,11 +1,14 @@
 import revestimientos1 from '../assets/Revestimientos/1.jpeg';
-import revestimientos2 from '../assets/Revestimientos/2.jpeg';
+import revestimientos2 from '../assets/Revestimientos/2.jpg';
 import revestimientos3 from '../assets/Revestimientos/3.jpeg';
 import revestimientos4 from '../assets/Revestimientos/4.jpg';
 import revestimientos5 from '../assets/Revestimientos/5.jpeg';
 import revestimientos6 from '../assets/Revestimientos/6.jpeg';
 import revestimientos7 from '../assets/Revestimientos/7.jpg';
-import revestimientos8 from '../assets/Revestimientos/8.jpeg';
+import revestimientos8 from '../assets/Revestimientos/8.jpg';
+import revestimientos9 from '../assets/Revestimientos/9.jpeg';
+import revestimientos10 from '../assets/Revestimientos/10.jpeg';
+import revestimientos11 from '../assets/Revestimientos/11.jpg';
 
 
 const revestimientosImages = [
@@ -17,6 +20,9 @@ const revestimientosImages = [
 	{ src: revestimientos6, alt: 'Revestimientos 6' },
 	{ src: revestimientos7, alt: 'Revestimientos 7' },
 	{ src: revestimientos8, alt: 'Revestimientos 8' },
+	{ src: revestimientos9, alt: 'Revestimientos 9' },
+	{ src: revestimientos10, alt: 'Revestimientos 10' },
+	{ src: revestimientos11, alt: 'Revestimientos 11' },
 	
 ];
 
